1c3d49e5b1222899555cbc604942c3e361cd24ec
Cezar Augusto <cezar.ags@icloud.com>
2020-10-07 19:45:52 -0300

    adicionado o metodo post para postagens

---
+++ PlanteUmaArvore-Front-Pi/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule} from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { FeedComponent } from './feed/feed.component';
+import { PostTemaComponent } from './post-tema/post-tema.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    SobreComponent,
+    ContatoComponent,
+    LoginComponent,
+    CadastroComponent,
+    PerfilComponent,
+    FeedComponent,
+    PostTemaComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PlanteUmaArvore-Front-Pi/src/app/service/postagem.service.ts
@@ -0,0 +1,33 @@
+  
+import { environment } from './../../environments/environment.prod';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllPostagens() {
+    return this.http.get('http://localhost:8080/postagens', this.token)
+  }
+
+  getByIdPostagem(id: number) {
+    return this.http.get(`http://localhost:8080/postagens/${id}`, this.token)
+  }
+  
+  getByTituloPostagem(titulo: string) {
+    return this.http.get(`http://localhost:8080/postagens/titulo/${titulo}`, this.token)
+  }
+
+  postPostagem(postagem: Postagem){
+    return this.http.post('http://localhost:8080/postagens', postagem, this.token)
+  }
+}
